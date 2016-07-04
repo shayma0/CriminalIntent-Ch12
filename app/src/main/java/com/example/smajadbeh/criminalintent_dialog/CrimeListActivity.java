c6760cb8f15688aa173047e4f41def0420297496
smajadbeh <shayma_s2012@hotmail.com>
2016-07-04 12:24:34 +0300

    init

---
+++ app/src/main/java/com/example/smajadbeh/criminalintent_dialog/CrimeListActivity.java
@@ -0,0 +1,11 @@
+package com.example.smajadbeh.criminalintent_dialog;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
